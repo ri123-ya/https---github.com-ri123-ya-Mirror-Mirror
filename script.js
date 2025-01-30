9182085be6f7221ca88efd1a3e234ed8fa3e7809
Riya Rastogi <riya02rastogi@gmail.com>
2025-01-30 08:59:31 +0530

    input

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 // The Mirror Mirror: Imagine you have a string, and you need to create a new string that is a mirror image of the original. Write a function that appends the reversed version of the original string to itself.
+
+let original = "Riya"
+let answer = mirrorString(original)
+console.log(answer)
